ab46f109f4c3e1e24cb1c31c6357184828b3a066
amteel543 <115514532+amteel543@users.noreply.github.com>
2023-03-09 21:57:36 -0800

    Updated with Project 6 solution

--- api/resources/brevets.py
+++ api/resources/brevets.py
@@ -7,23 +7,23 @@ from flask_restful import Resource
 # You need to implement this in database/models.py
 from database.models import Brevet
 
-# MongoEngine queries:
-# Brevet.objects() : similar to find_all. Returns a MongoEngine query
-# Brevet(...).save() : creates new brevet
-# Brevet.objects.get(id=...) : similar to find_one
+class Brevets(Resource):
 
-# Two options when returning responses:
-#
-# return Response(json_object, mimetype="application/json", status=200)
-# return python_dict, 200
-#
-# Why would you need both?
-# Flask-RESTful's default behavior:
-# Return python dictionary and status code,
-# it will serialize the dictionary as a JSON.
-#
-# MongoEngine's objects() has a .to_json() but not a .to_dict(),
-# So when you're returning a brevet / brevets, you need to convert
-# it from a MongoEngine query object to a JSON and send back the JSON
-# directly instead of letting Flask-RESTful attempt to convert it to a
-# JSON for you.
+    def get(self):
+
+        output_json = Brevet.objects().to_json()
+
+        return Response(output_json, mimetype="application/json", status=200)
+
+    def post(self):
+
+        input_json = request.json
+
+        length = input_json["length"]
+        
+        start_time = input_json["start_time"]
+
+        checkpoints = input_json["checkpoints"]
+
+        result = Brevet(length=length, start_time=start_time, checkpoints=checkpoints).save()
+        return {'_id': str(result.id)}, 200
